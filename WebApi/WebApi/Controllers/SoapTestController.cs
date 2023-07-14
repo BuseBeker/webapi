1004868e000984be990be151cfee01b9bcdbc334
Buse Latife BEKER <busebeker.bb@gmail.com>
2023-07-14 15:11:27 +0300

    added PersonController, StudentController,  N11ProductServices, SoapTestController added

---
+++ WebApi/WebApi/Controllers/SoapTestController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using N11ProductService;
+
+namespace WebApi.Controllers;
+
+
+[ApiController]
+[Route("webApi/api/[controller]")]
+public class SoapTestController : ControllerBase
+{
+    public SoapTestController()
+    {
+
+    }
+
+
+    [HttpDelete("{id}")]
+    public async void DeleteProductById(int id)
+    {
+        ProductServicePortClient productService = new ProductServicePortClient();
+        var request = new DeleteProductByIdRequest();
+        request.auth = new Authentication()
+        {
+            appKey = "appkey",
+            appSecret = "appsecret"
+        };
+        request.productId = id;
+        var response = await productService.DeleteProductByIdAsync(request);
+        if (response.DeleteProductByIdResponse.result.errorCode != "0")
+        {
+
+        }
+    }
+}
